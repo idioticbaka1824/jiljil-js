26aa0c776a4505f072a6278a996b723d116df3dc
raadshaikh <raadshaikh2000@gmail.com>
2025-01-10 23:57:24 -0600

    .

--- jiljil-ui.js
+++ jiljil-ui.js
@@ -175,12 +175,12 @@
             }
 			if (rtl==false) {
 				for (let i = 0; i < str.length; i++) {
-					this.ctx.drawImage(this.bmp_jiljil, (str.charCodeAt(i) - 0x30) * 8, 112, 8, 16, x + 8*i, y, 8, 16);
+					this.ctx.drawImage2(this.bmp_jiljil, (str.charCodeAt(i) - 0x30) * 8, 112, 8, 16, x + 8*i, y, 8, 16);
 				}
 			}
 			else if(rtl==true) //right-to-left, for right-aligned numbers
 				for (let i = str.length-1; i >= 0; i--) {
-					this.ctx.drawImage(this.bmp_jiljil, (str.charCodeAt(i) - 0x30) * 8, 112, 8, 16, x - 8*(str.length-i), y, 8, 16);
+					this.ctx.drawImage2(this.bmp_jiljil, (str.charCodeAt(i) - 0x30) * 8, 112, 8, 16, x - 8*(str.length-i), y, 8, 16);
 				}
         }
 		
@@ -189,7 +189,7 @@
 			for(let i=0; i<str.length; i++){
 				if(str[i]=='\n'){newlines.push(i);}
 				if(str.charCodeAt(i)>=0x20){
-					this.ctx.drawImage(this.bmp_charactor, 8*((str.charCodeAt(i)-0x20)%10), 8*~~((str.charCodeAt(i)-0x20)/10), 8, 8, x+6*(i-newlines[newlines.length-1]-(newlines.length>1))*zoom, y+8*(newlines.length-1)*zoom, 8*zoom, 8*zoom); //~~ is shortcut for floor function somehow
+					this.ctx.drawImage2(this.bmp_charactor, 8*((str.charCodeAt(i)-0x20)%10), 8*~~((str.charCodeAt(i)-0x20)/10), 8, 8, x+6*(i-newlines[newlines.length-1]-(newlines.length>1))*zoom, y+8*(newlines.length-1)*zoom, 8*zoom, 8*zoom); //~~ is shortcut for floor function somehow
 				}
 			}
 		}
@@ -212,26 +212,26 @@
 				this.ctx2.fillstyle = 'black';
 				this.ctx2.fillRect(0, 0, width, height);
 				for(let i=0; i<=15; i++){
-					// this.ctx2.drawImage(this.bmp_jiljil, 64, 64, 20, 20, i*20, 0, 20, 20); //ceiling
-					// this.ctx2.drawImage(this.bmp_jiljil, 64, 64, 20, 20, i*20, 240-20, 20, 20); //floor
-					this.ctx2.drawImage(this.bmp_jiljil, 64, 64, 20, 20, 0, i*20, 20, 20); //left wall
-					this.ctx2.drawImage(this.bmp_jiljil, 64, 64, 20, 20, 320-20, i*20, 20, 20); //left wall
+					// this.ctx2.drawImage2(this.bmp_jiljil, 64, 64, 20, 20, i*20, 0, 20, 20); //ceiling
+					// this.ctx2.drawImage2(this.bmp_jiljil, 64, 64, 20, 20, i*20, 240-20, 20, 20); //floor
+					this.ctx2.drawImage2(this.bmp_jiljil, 64, 64, 20, 20, 0, i*20, 20, 20); //left wall
+					this.ctx2.drawImage2(this.bmp_jiljil, 64, 64, 20, 20, 320-20, i*20, 20, 20); //left wall
 				}
-				// this.ctx2.drawImage(this.bmp_touchUI, 0, 0, 320, 240, 0, 0, 320, 240);
-				this.ctx2.drawImage(this.bmp_touchUI, 0, 0, 32, 32, 32, 8, 32, 32); //esc button
-				this.ctx2.drawImage(this.bmp_touchUI, 0, 72, 32, 32, 32, 200, 32, 32); //z button
-				this.ctx2.drawImage(this.bmp_touchUI, 88, 0, 196, 196, 61, 21, 196, 196); //tan circle
+				// this.ctx2.drawImage2(this.bmp_touchUI, 0, 0, 320, 240, 0, 0, 320, 240);
+				this.ctx2.drawImage2(this.bmp_touchUI, 0, 0, 32, 32, 32, 8, 32, 32); //esc button
+				this.ctx2.drawImage2(this.bmp_touchUI, 0, 72, 32, 32, 32, 200, 32, 32); //z button
+				this.ctx2.drawImage2(this.bmp_touchUI, 88, 0, 196, 196, 61, 21, 196, 196); //tan circle
 				for(let i=0; i<8; i++){ //compass rose
 					var x=160+88*Math.cos(i*2*Math.PI/8); //radius is nominally 90 but it was looking a little wonky so slight adjustments here
 					var y=120-89*Math.sin(i*2*Math.PI/8);
 					var size=i%2?6:9;
-					this.ctx2.drawImage(this.bmp_touchUI, 0, i%2?65:56, size, size, ~~(x-size/2), ~~(y-size/2), size, size);
+					this.ctx2.drawImage2(this.bmp_touchUI, 0, i%2?65:56, size, size, ~~(x-size/2), ~~(y-size/2), size, size);
 				}
 				if(this.game.gameState!='playing'){
-					this.ctx2.drawImage(this.bmp_touchUI, 32, 0, 56, 56, 132, 92, 56, 56); //space button
+					this.ctx2.drawImage2(this.bmp_touchUI, 32, 0, 56, 56, 132, 92, 56, 56); //space button
 				}
 				if(this.game.gameState=='playing'){
-					this.ctx2.drawImage(this.bmp_touchUI, 9, 56, 5, 5, 158, 117, 5, 5); //compass centre dot
+					this.ctx2.drawImage2(this.bmp_touchUI, 9, 56, 5, 5, 158, 117, 5, 5); //compass centre dot
 					this.UIhead_x = 1*window.keysBeingPressed['ArrowRight'] + -1*window.keysBeingPressed['ArrowLeft'];
 					this.UIhead_y = 1*window.keysBeingPressed['ArrowUp'] + -1*window.keysBeingPressed['ArrowDown'];
 					this.UIhead_x *= 89/(this.UIhead_x&&this.UIhead_y?2**0.5:1);
@@ -239,7 +239,7 @@
 					this.UIhead_x = 160+this.UIhead_x-16/2;
 					this.UIhead_y = 120-this.UIhead_y-16/2;
 					// if(window.keysBeingPressed['ArrowRight']||window.keysBeingPressed['ArrowUp']||window.keysBeingPressed['ArrowLeft']||window.keysBeingPressed['ArrowDown']){
-						this.ctx2.drawImage(this.bmp_jiljil, 0, 0, 16, 16, ~~this.UIhead_x, ~~this.UIhead_y, 16, 16);
+						this.ctx2.drawImage2(this.bmp_jiljil, 0, 0, 16, 16, ~~this.UIhead_x, ~~this.UIhead_y, 16, 16);
 					// }
 				}
 				if(this.game.gameState=='gameover'){
@@ -250,7 +250,7 @@
 					if(this.UIhead_x==160-16/2 && this.UIhead_y==120-16/2){
 						this.UIhead_y -= 16;
 					}
-					this.ctx2.drawImage(this.bmp_jiljil, 64, 16, 16, 16, ~~this.UIhead_x, ~~this.UIhead_y, 16, 16);
+					this.ctx2.drawImage2(this.bmp_jiljil, 64, 16, 16, 16, ~~this.UIhead_x, ~~this.UIhead_y, 16, 16);
 				}
 					
 			}
@@ -258,27 +258,27 @@
 			switch(this.game.gameState) {
 				case 'loading':
 					this.drawString(130,window.height/2-4,'...LOADED!');
-					this.ctx.drawImage(this.bmp_jiljil, 64, 36, 64, 12, 130, 164, 64, 12); //'Push Space'
+					this.ctx.drawImage2(this.bmp_jiljil, 64, 36, 64, 12, 130, 164, 64, 12); //'Push Space'
 					break;
 					
 				case 'startscreen':
 					this.stop_bgm();
 					if(this.frameCount==24){this.se[7].play();}
 					if(this.frameCount>56){
-						this.ctx.drawImage(this.bmp_jiljil, 88, 64, 36, 20, 124, 50, 36, 20); //'JiL'
-						this.ctx.drawImage(this.bmp_jiljil, 88, 64, 36, 20, 124+36, 50, 36, 20); //'JiL'
+						this.ctx.drawImage2(this.bmp_jiljil, 88, 64, 36, 20, 124, 50, 36, 20); //'JiL'
+						this.ctx.drawImage2(this.bmp_jiljil, 88, 64, 36, 20, 124+36, 50, 36, 20); //'JiL'
 					}
 					if(this.frameCount==56){this.se[8].play();}
 					if(this.frameCount>90){
-						this.ctx.drawImage(this.bmp_jiljil, 80, 24, 48, 8, 137, 119, 48, 8); //'1997-10-xx'
+						this.ctx.drawImage2(this.bmp_jiljil, 80, 24, 48, 8, 137, 119, 48, 8); //'1997-10-xx'
 					}
 					if(this.frameCount==90){this.se[9].play();}
 					if(this.frameCount>125){
-						this.ctx.drawImage(this.bmp_jiljil, 64, 102, 56, 10, 132, 139, 56, 10); //'Tortoiseshell'
+						this.ctx.drawImage2(this.bmp_jiljil, 64, 102, 56, 10, 132, 139, 56, 10); //'Tortoiseshell'
 					}
 					if(this.frameCount==125){this.se[10].play();}
 					if(this.frameCount>172){
-						this.ctx.drawImage(this.bmp_jiljil, 64, 36, 64, 12, 130, 164, 64, 12); //'Push Space'
+						this.ctx.drawImage2(this.bmp_jiljil, 64, 36, 64, 12, 130, 164, 64, 12); //'Push Space'
 					}
 					if(this.frameCount==172){this.se[11].play();}
 					this.frameCount += 1;
@@ -291,7 +291,7 @@
 						this.bgm1_playing = true;
 					}
 					
-					// this.ctx.drawImage(this.bmp_jiljil, 0, 85, 2, 2, this.game.playerCurPos.x-2/2, this.game.playerCurPos.y-2/2, 2, 2); //dot that represents 'player cursor', see jiljil.js for details. comment out when finished
+					// this.ctx.drawImage2(this.bmp_jiljil, 0, 85, 2, 2, this.game.playerCurPos.x-2/2, this.game.playerCurPos.y-2/2, 2, 2); //dot that represents 'player cursor', see jiljil.js for details. comment out when finished
 					
 					this.frameCount += 1;
 					break;
@@ -303,20 +303,20 @@
 						this.bgm2_playing = true;
 					}
 					
-					this.ctx.drawImage(this.bmp_jiljil, 64, 36, 64, 12, 48, 48, 64, 12); //'push space'
+					this.ctx.drawImage2(this.bmp_jiljil, 64, 36, 64, 12, 48, 48, 64, 12); //'push space'
 					break;
 					
 				case 'escmenu':
 					// this.bgm1.pause();
 					// this.bgm2.pause();
-					// this.ctx.drawImage(this.bmp_escape, 0, 0, 80, 16, 8, 8, 80, 16); //pause screen
+					// this.ctx.drawImage2(this.bmp_escape, 0, 0, 80, 16, 8, 8, 80, 16); //pause screen
 					this.drawString(0,0,'CONTINUE:F');
 					this.drawString(0,8,'RESET   :G');
 					this.drawString(0,16,'HELP    :H');
 					if ('ontouchstart' in window) {
-						this.ctx.drawImage(this.bmp_touchUI, 0, 104, 68, 68, 90, 14, 68, 68); //bubbles around F, G, H
-						this.ctx.drawImage(this.bmp_touchUI, 0, 104, 68, 68, 90+80*1+2, 14, 68, 68);
-						this.ctx.drawImage(this.bmp_touchUI, 0, 104, 68, 68, 90+80*2+1, 14, 68, 68);
+						this.ctx.drawImage2(this.bmp_touchUI, 0, 104, 68, 68, 90, 14, 68, 68); //bubbles around F, G, H
+						this.ctx.drawImage2(this.bmp_touchUI, 0, 104, 68, 68, 90+80*1+2, 14, 68, 68);
+						this.ctx.drawImage2(this.bmp_touchUI, 0, 104, 68, 68, 90+80*2+1, 14, 68, 68);
 						this.ctx.filter = 'brightness(50%)';
 						this.drawString(104,28,'F', 5);
 						this.drawString(104+80*1,28,'G', 5);
@@ -334,25 +334,25 @@
 			
 			if(this.game.gameState == 'playing' || this.game.gameState == 'gameover'){
 				for(let i=1; i<=14; i++){
-					this.ctx.drawImage(this.bmp_jiljil, 64, 64, 20, 20, i*20, 0, 20, 20); //ceiling
-					this.ctx.drawImage(this.bmp_jiljil, 64, 64, 20, 20, i*20, 240-20, 20, 20); //floor
+					this.ctx.drawImage2(this.bmp_jiljil, 64, 64, 20, 20, i*20, 0, 20, 20); //ceiling
+					this.ctx.drawImage2(this.bmp_jiljil, 64, 64, 20, 20, i*20, 240-20, 20, 20); //floor
 				}
-				this.ctx.drawImage(this.bmp_jiljil, 0, 48, 64, 64, this.game.lemonPos.x-64/2, this.game.lemonPos.y-64/2, 64, 64); //lemon
+				this.ctx.drawImage2(this.bmp_jiljil, 0, 48, 64, 64, this.game.lemonPos.x-64/2, this.game.lemonPos.y-64/2, 64, 64); //lemon
 				for(let i=6; i>=0; i--){ //worm segments
-						this.ctx.drawImage(this.bmp_jiljil, 16*(i+1), 0, 16, 16, this.game.playerSegPos[i].x-16/2, this.game.playerSegPos[i].y-16/2, 16, 16);
+						this.ctx.drawImage2(this.bmp_jiljil, 16*(i+1), 0, 16, 16, this.game.playerSegPos[i].x-16/2, this.game.playerSegPos[i].y-16/2, 16, 16);
 				}
-				this.ctx.drawImage(this.bmp_jiljil, 64*(this.game.gameState=='gameover'), 16*(this.game.gameState=='gameover'), 16, 16, this.game.playerPos.x-16/2, this.game.playerPos.y-16/2, 16, 16); //worm head (crying or not depending on gameover)
-				this.ctx.drawImage(this.bmp_jiljil, 0, 16, 32, 32, this.game.paw0Pos.x-32/2, this.game.paw0Pos.y-32/2, 32, 32); //pawprints
-				this.ctx.drawImage(this.bmp_jiljil, 32, 16, 32, 32, this.game.paw1Pos.x-32/2, this.game.paw1Pos.y-32/2, 32, 32); //pawprints
+				this.ctx.drawImage2(this.bmp_jiljil, 64*(this.game.gameState=='gameover'), 16*(this.game.gameState=='gameover'), 16, 16, this.game.playerPos.x-16/2, this.game.playerPos.y-16/2, 16, 16); //worm head (crying or not depending on gameover)
+				this.ctx.drawImage2(this.bmp_jiljil, 0, 16, 32, 32, this.game.paw0Pos.x-32/2, this.game.paw0Pos.y-32/2, 32, 32); //pawprints
+				this.ctx.drawImage2(this.bmp_jiljil, 32, 16, 32, 32, this.game.paw1Pos.x-32/2, this.game.paw1Pos.y-32/2, 32, 32); //pawprints
 				if(this.game.gameState=='playing'){//sparkle
-					this.ctx.drawImage(this.bmp_jiljil, 64+16*(this.game.sparkleFrame), 48, 16, 16, this.game.sparklePos.x-16/2, this.game.sparklePos.y-16/2, 16, 16);
+					this.ctx.drawImage2(this.bmp_jiljil, 64+16*(this.game.sparkleFrame), 48, 16, 16, this.game.sparklePos.x-16/2, this.game.sparklePos.y-16/2, 16, 16);
 				}
-				this.ctx.drawImage(this.bmp_jiljil, 63, 94, 24, 8, 20, 211, 24, 8); //'teema:'
-				this.ctx.drawImage(this.bmp_jiljil, 64, 84, 64, 8, 48, 211, 64, 8); //'shippo ga abunai'
-				this.ctx.drawImage(this.bmp_jiljil, 80, 16, 48, 8, 140, 223, 48, 8); //'esc->exit'
-				this.ctx.drawImage(this.bmp_jiljil, 80, 120, 48, 8, 225, 211, 48, 8); //'score'
-				this.ctx.drawImage(this.bmp_jiljil, 80, 120, 48, 8, 225, 20, 48, 8); //'score'
-				this.ctx.drawImage(this.bmp_jiljil, 80, 112, 18, 8, 207, 21, 18, 8); //'hi'
+				this.ctx.drawImage2(this.bmp_jiljil, 63, 94, 24, 8, 20, 211, 24, 8); //'teema:'
+				this.ctx.drawImage2(this.bmp_jiljil, 64, 84, 64, 8, 48, 211, 64, 8); //'shippo ga abunai'
+				this.ctx.drawImage2(this.bmp_jiljil, 80, 16, 48, 8, 140, 223, 48, 8); //'esc->exit'
+				this.ctx.drawImage2(this.bmp_jiljil, 80, 120, 48, 8, 225, 211, 48, 8); //'score'
+				this.ctx.drawImage2(this.bmp_jiljil, 80, 120, 48, 8, 225, 20, 48, 8); //'score'
+				this.ctx.drawImage2(this.bmp_jiljil, 80, 112, 18, 8, 207, 21, 18, 8); //'hi'
 				this.drawNumber(275, 203, this.game.score, 3); //score
 				this.drawNumber(275, 20, this.game.highscore, 3); //high score
 				
@@ -362,7 +362,7 @@
 				}
 				
 			}
-			if(this.game.gameState == 'gameover'){this.ctx.drawImage(this.bmp_jiljil, 64, 36, 64, 12, 48, 48, 64, 12);}
+			if(this.game.gameState == 'gameover'){this.ctx.drawImage2(this.bmp_jiljil, 64, 36, 64, 12, 48, 48, 64, 12);}
         }
     }
 
